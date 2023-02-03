a36de59f72f90174912080cc5cad1d6f925d8a0a
Cande <cande_kn@hotmail.com.ar>
2023-02-03 14:30:50 -0300

    modifico nosotros

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import logoColor from "../../assets/img/logo-color.png";
+import { FaShippingFast, FaPaperPlane  } from "react-icons/fa";
+import dancing from "../../assets/img/dancing.gif";
 
 export const Nosotros = () => {
     return (
         <Container>
-            <article className='m-5'>
-                <h3>
-                    Somos <strong className='text-primary'>TANGERINE GAMES</strong>, una tienda argentina de videojuegos, fundada en 2020. 
-                    <br />
-                </h3>
+        <Row className='mx-5'>
+            <Col lg={3} className="d-flex flex-column justify-content-end align-items-center">
+            <Image src={dancing} width={150} />
+            </Col>
+            <Col lg={6}className="d-flex flex-column justify-content-start align-items-center">
+            <h1 className='text-primary my-3'>Tangerine Games</h1>
                 <h4> 
-                    Ofrecemos los mejores precios del mercado en
-                    videojuegos tanto fisicos como digitales.
-                    No tenemos una sede física donde visitarnos. Pero contamos con muchos sistemas de envíos para que puedas recibir tu producto desde
-                    cualquier parte del país.
-                    Y, en caso de ser de un país diferente a Argentina, ¡No te preocupes! comprando en formato digital, aún puedes recibir tu videojuego favorito, estés donde estés.
+                    Somos la mejor tienda argentina de videojuegos </h4>
+                    <h4>Con los precios más bajos del mercado
                 </h4>
+            </Col>
+            <Col lg={3} className="d-flex justify-content-start align-items-center my-3">
+                <Image src={logoColor} width={150} />
+            </Col>
+        </Row>
+            <article className='m-5 text-center'>
+ 
+                <h5>
+                    <FaShippingFast color='#f28500' size={80}/> Contamos con muchos sistemas de envíos para que puedas recibir tu producto desde
+                    cualquier parte del país. <FaShippingFast color="#f28500" size={80} /> 
+                </h5>
+                <h5>
+                    Y, en caso de ser de un país diferente a Argentina... 
+                </h5>
+                <h5>
+                    ¡No te preocupes! comprando en formato digital, aún puedes recibir tu videojuego favorito, estés donde estés. <FaPaperPlane color='#f28500' size={25} />
+                </h5>
             </article>
         </Container>
     )
